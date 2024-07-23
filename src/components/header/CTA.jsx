7f948dce260c9227d5d749d696aa0723795c33f7
oyezaheer <zaheer286khan@gmail.com>
2024-07-23 20:40:42 +0530

    resume updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+// import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className = 'cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href="https://drive.google.com/file/d/1pr87aVUtqfcNEpfrOSYvFz_JWW2niFJF/view?usp=drive_link" download className='btn'>RESUME</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
