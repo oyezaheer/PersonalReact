2bcdefb3c0a1c3fc4ec681b8c8d3bc77d285ef5a
oyezaheer <zaheer286khan@gmail.com>
2024-08-27 10:40:54 +0530

    add leetcode profile

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
 import {BsInstagram} from 'react-icons/bs'
+import {SiLeetcode} from 'react-icons/si'
+
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin /></a>
-        <a href="https://github.com" target="_blank" rel="noreferrer"><FaGithub /></a>
-        <a href="https://instagram.com" target="_blank" rel="noreferrer"><BsInstagram /></a>
+        <a href="https://leetcode.com/u/oyezaheer/" target="_blank" rel="noreferrer"><SiLeetcode /></a>
+        <a href="https://www.linkedin.com/in/zaheer-khan-3b222019b" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+        <a href="https://github.com/oyezaheer" target="_blank" rel="noreferrer"><FaGithub /></a>
+        <a href="https://instagram.com/oyezaheer" target="_blank" rel="noreferrer"><BsInstagram /></a>
       
     </div>
   )
